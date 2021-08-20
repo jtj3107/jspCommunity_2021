936728b205d3d66c44d68018665c8ae6767e59ae
jtj3107 <jtj3926@gmail.com>
2021-08-20 10:28:17 +0900

    회원 리스트를 사용자 페이지가 아닌 관리자 페이지로 옮김

---
+++ src/main/java/com/jtj/example/jspCommunity/servlet/UsrDispatcherServlet.java
@@ -0,0 +1,81 @@
+package com.jtj.example.jspCommunity.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jtj.example.jspCommunity.container.Container;
+import com.jtj.example.jspCommunity.controller.usr.ArticleController;
+import com.jtj.example.jspCommunity.controller.usr.MemberController;
+import com.sbs.mysqliutil.MysqlUtil;
+
+@WebServlet("/usr/*")
+public class UsrDispatcherServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+
+		String requestUri = req.getRequestURI();
+		String[] requestUriBits = requestUri.split("/");
+
+		if (requestUriBits.length < 5) {
+			resp.getWriter().append("올바른 요청이 아닙니다.");
+			return;
+		}
+
+		MysqlUtil.setDBInfo("127.0.0.1", "geotjeoli", "gjl123414", "jspCommunity");
+
+		String jspPath = null;
+
+		String controllerName = requestUriBits[3];
+		String actionMethodName = requestUriBits[4];
+
+		if (controllerName.equals("member")) {
+			MemberController memberController = Container.memberController;
+
+			if (actionMethodName.equals("list")) {
+				jspPath = memberController.showList(req, resp);
+			}
+		} else if (controllerName.equals("article")) {
+			ArticleController articleController = Container.articleController;
+
+			if (actionMethodName.equals("list")) {
+				jspPath = articleController.showList(req, resp);
+			}
+			else if(actionMethodName.equals("detail")) {
+				jspPath = articleController.showDetail(req, resp);
+			}
+			else if(actionMethodName.equals("write")) {
+				jspPath = articleController.showWrite(req, resp);
+			}
+			else if(actionMethodName.equals("doWrite")) {
+				jspPath = articleController.doWrite(req, resp);
+			}
+			else if(actionMethodName.equals("doDelete")) {
+				jspPath = articleController.doDelete(req, resp);
+			}
+			else if(actionMethodName.equals("modify")) {
+				jspPath = articleController.showModify(req, resp);
+			}
+			else if(actionMethodName.equals("doModify")) {
+				jspPath = articleController.doModify(req, resp);
+			}
+		}
+
+		MysqlUtil.closeConnection();
+
+		RequestDispatcher rd = req.getRequestDispatcher("/jsp/" + jspPath + ".jsp");
+		rd.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
